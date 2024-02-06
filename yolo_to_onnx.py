92a8d97e676539512570e57225f5268c925226ff
Sachitanmuo <k9764315@gmail.com>
2024-02-06 11:09:54 +0800

    backup

---
+++ yolo_to_onnx.py
@@ -0,0 +1,13 @@
+import torch
+from yolo.yolo import cv_Yolo 
+import os
+yolo_path = os.path.abspath(os.path.dirname(__file__)) + '/weights'
+model = cv_Yolo (yolo_path)
+model.load_state_dict(torch.load('yolov8n.pt'))
+model.eval()
+
+
+dummy_input = torch.randn(1, 3, 416, 416)  
+
+onnx_path = 'yolo_model.onnx'
+torch.onnx.export(model, dummy_input, onnx_path, input_names=['input'], output_names=['output'])
